70482d29a4a67d60a83b78f3ba707f252bf5083d
Denys Yatsuliak <comdenchik@gmail.com>
2023-06-22 13:01:52 +0300

    Added documentation with help of ChatGPT

--- WatchFolderManager.cs
+++ WatchFolderManager.cs
@@ -31,10 +31,21 @@ using System.Linq;
 
 namespace ShareX
 {
+    /// <summary>
+    /// Manages a collection of watched folders. Provides functionality to add, remove, 
+    /// and update the folders in the watch list, and to control their active state.
+    /// </summary>
     public class WatchFolderManager : IDisposable
     {
+        /// <summary>
+        /// List of folders currently being watched.
+        /// </summary>
         public List<WatchFolder> WatchFolders { get; private set; }
 
+        /// <summary>
+        /// Updates the current list of watched folders. Any existing folders in the list 
+        /// are unregistered before the list is refreshed.
+        /// </summary>
         public void UpdateWatchFolders()
         {
             if (WatchFolders != null)
@@ -58,16 +69,36 @@ namespace ShareX
             }
         }
 
+        /// <summary>
+        /// Finds a watched folder in the list that matches the provided settings. 
+        /// </summary>
+        /// <param name="watchFolderSetting">The settings of the watched folder to find.</param>
+        /// <returns>The watched folder if it exists, otherwise null.</returns>
         private WatchFolder FindWatchFolder(WatchFolderSettings watchFolderSetting)
         {
             return WatchFolders.FirstOrDefault(watchFolder => watchFolder.Settings == watchFolderSetting);
         }
 
+        /// <summary>
+        /// Checks if a folder with the provided settings exists in the watched folders list.
+        /// </summary>
+        /// <param name="watchFolderSetting">The settings of the watched folder to find.</param>
+        /// <returns>True if a watched folder with the given settings exists in the list, otherwise false.</returns>
         private bool IsExist(WatchFolderSettings watchFolderSetting)
         {
             return FindWatchFolder(watchFolderSetting) != null;
         }
 
+        /// <summary>
+        /// Adds a new watched folder to the list. If a folder with the same settings 
+        /// already exists in the list, this method will not add a duplicate. If the 
+        /// watched folder list of the provided task settings does not contain the 
+        /// new watched folder setting, it will be added. 
+        /// </summary>
+        /// <param name="watchFolderSetting">The settings for the new watched folder.</param>
+        /// <param name="taskSettings">The task settings for the new watched folder. 
+        /// These settings will be copied to ensure the original settings are not 
+        /// unintentionally modified by the watched folder.</param>
         public void AddWatchFolder(WatchFolderSettings watchFolderSetting, TaskSettings taskSettings)
         {
             if (!IsExist(watchFolderSetting))
@@ -107,6 +138,11 @@ namespace ShareX
             }
         }
 
+        /// <summary>
+        /// Removes a watched folder from the list. If the folder exists in the list, 
+        /// it will also be removed from the watched folder list of the associated task settings.
+        /// </summary>
+        /// <param name="watchFolderSetting">The settings of the watched folder to remove.</param>
         public void RemoveWatchFolder(WatchFolderSettings watchFolderSetting)
         {
             using (WatchFolder watchFolder = FindWatchFolder(watchFolderSetting))
@@ -119,6 +155,12 @@ namespace ShareX
             }
         }
 
+        /// <summary>
+        /// Updates the active state of a watched folder to match its associated task settings. 
+        /// If the task settings indicate the folder should be active, this method will enable it; 
+        /// otherwise, it will be disposed.
+        /// </summary>
+        /// <param name="watchFolderSetting">The settings of the watched folder to update.</param>
         public void UpdateWatchFolderState(WatchFolderSettings watchFolderSetting)
         {
             WatchFolder watchFolder = FindWatchFolder(watchFolderSetting);
@@ -135,6 +177,10 @@ namespace ShareX
             }
         }
 
+        /// <summary>
+        /// Unregisters all folders in the watched folders list.
+        /// If the list is null or empty, the method does nothing.
+        /// </summary>
         public void UnregisterAllWatchFolders()
         {
             if (WatchFolders != null)
@@ -149,6 +195,9 @@ namespace ShareX
             }
         }
 
+        /// <summary>
+        /// Disposes the WatchFolderManager, which includes unregistering all watched folders.
+        /// </summary>
         public void Dispose()
         {
             UnregisterAllWatchFolders();
